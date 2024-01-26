af78156c20e0999e332b57a79a1235d983fc7e0b
ColorXJH <1204332316@qq.com>
2024-01-26 08:25:22 +0800

    test

---
+++ src/main/java/com/master/PART3/Test3.java
@@ -0,0 +1,10 @@
+package com.master.PART3;
+
+/**
+ * @author ColorXJH
+ * @version 1.0
+ * @description:
+ * @date 2024-01-26 8:24
+ */
+public class Test3 {
+}
---
+++ src/main/java/com/master/PART4/Test4.java
@@ -0,0 +1,10 @@
+package com.master.PART4;
+
+/**
+ * @author ColorXJH
+ * @version 1.0
+ * @description:
+ * @date 2024-01-26 8:25
+ */
+public class Test4 {
+}
