bdced34feb57a972178b302e1f3421a9277fcfb5
ColorXJH <1204332316@qq.com>
2024-05-17 15:27:22 +0800

    170

--- src/main/java/com/master/PART3/RebuildClass.java
+++ src/main/java/com/master/PART3/RebuildClass.java
@@ -1,5 +1,7 @@
 package com.master.PART3;
 
+import com.sun.corba.se.impl.orbutil.concurrent.Sync;
+
 import java.util.Hashtable;
 
 /**
@@ -158,5 +160,261 @@ public class RebuildClass {
     //构建子类
         //读出者与写入者：是这样一组并发构建：他么有着相同的基础，但是一个负责控制那些执行读出操作的线程，另一个负责控制那些执行写入操作的线程
         //他们并发控制的策略不一样
+    //以下版本是通用的，其子类基本不需要做太多的修改，值得一提的是，对等待读出者的计数在这个版本中实际上是没有必要的,因为实际上并没有依赖于他的策略，
+    //但是由于它的存在，你可以通过让allowReader和allowWriter方法中的谓词依赖于这个值，来调整控制策略，例如，你可以修改这些方法中的条件，来决定
+    //优先处理等待队列中计数器值更大的线程
+    abstract class ReadWrite{
+        protected int activeReaders=0;//threads executing reader
+        protected int activeWriters=0;//always zero or one
+        protected int waitingReaders=0;//thread not yet in read
+        protected int waitingWriters=0;//some for write
+        protected abstract void doRead();//implement in subclasses
+        protected abstract void doWrite();
+        public void read()throws InterruptedException{
+            beforeRead();
+            try {
+                doRead();
+            }finally {
+                afterRead();
+            }
+        }
+        public void write()throws InterruptedException{
+            beforeWrite();
+            try{
+                doWrite();
+            }finally {
+                afterWrite();
+            }
+        }
+        //如果当前有等待中的写入者，读出者线程将被阻塞，写入者不可以降级为读出者
+        protected boolean allowReader(){
+            return waitingWriters==0&&activeWriters==0;
+        }
+        //如果当前存在活动的读出者线程和写人者线程，该写入者线程将被阻塞
+        protected boolean allowWriter(){
+            return activeReaders==0&&activeWriters==0;
+        }
+        protected synchronized void beforeRead() throws InterruptedException{
+            ++waitingReaders;
+            while (!allowReader()){
+                try{
+                    wait();
+                }catch (InterruptedException ie){
+                    --waitingReaders;//rollback state
+                    throw ie;
+                }
+            }
+            --waitingReaders;
+            ++activeReaders;
+        }
+        protected synchronized void afterRead(){
+            --activeReaders;
+            notifyAll();
+        }
+        protected synchronized void beforeWrite()throws InterruptedException{
+            ++waitingWriters;
+            while (!allowWriter()){
+                try {
+                    wait();
+                }catch (InterruptedException ie){
+                    --waitingWriters;
+                    throw ie;
+                }
+            }
+            --waitingWriters;
+            ++activeWriters;
+        }
+        protected synchronized void afterWrite(){
+            --activeWriters;
+            notifyAll();
+        }
+
+    }
+    //这个类或它的子类可以被重新包装以下以支持ReadWriteLock接口，可以通过内部类来实现（类似的策略也被用在juc的ReadWriteLock中，不过其中包含了一些技巧来避免不必要的通知）
+    interface MyReadWriteLock{
+        Sync readLock();
+        Sync writeLock();
+    }
+    class RWLock extends ReadWrite implements MyReadWriteLock {
+        class RLock implements Sync {
+
+            @Override
+            public void acquire() throws InterruptedException {
+                beforeRead();
+            }
+
+            @Override
+            public boolean attempt(long msecs) throws InterruptedException {
+                return beforeReade(msecs);
+            }
+
+            @Override
+            public void release() {
+                afterRead();
+            }
+        }
+
+        class WLock implements Sync{
+
+            @Override
+            public void acquire() throws InterruptedException {
+                beforeWrite();
+            }
+
+            @Override
+            public boolean attempt(long msecs) throws InterruptedException {
+                return beforeWrite(msecs);
+            }
+
+            @Override
+            public void release() {
+                afterWrite();
+            }
+        }
+        public boolean beforeReade(long times) throws InterruptedException{
+            ++waitingReaders;
+            while (!allowReader()){
+                try{
+                    wait();
+                }catch (InterruptedException ie){
+                    --waitingReaders;//rollback state
+                    throw ie;
+                }
+            }
+            --waitingReaders;
+            ++activeReaders;
+            return true;
+        }
+        public boolean beforeWrite(long times) throws InterruptedException{
+            ++waitingWriters;
+            while (!allowWriter()){
+                try {
+                    wait();
+                }catch (InterruptedException ie){
+                    --waitingWriters;
+                    throw ie;
+                }
+            }
+            --waitingWriters;
+            ++activeWriters;
+            return true;
+        }
+        @Override
+        protected void doRead() {
+            System.out.println("read is running...");
+        }
+
+        @Override
+        protected void doWrite() {
+            System.out.println("write is running...");
+        }
+        protected final RLock rlock=new RLock();
+        protected final WLock wLock=new WLock();
+
+        @Override
+        public Sync readLock() {
+            return rlock;
+        }
+
+        @Override
+        public Sync writeLock() {
+            return wLock;
+        }
+    }
+
+    //将保障分层
+        //保障可以被添加到那些被编写为阻碍模式的基本数据结构中
+    class StackEmptyException extends Exception{}
+    class Stack{
+        public synchronized boolean isEmpty(){
+            //blablabla
+            return true;
+        }
+        public synchronized void push(Object x){
+            //blablabla
+        }
+        public synchronized Object pop()throws StackEmptyException{
+            if(isEmpty()){
+                throw new StackEmptyException();
+            }
+            return null;
+        }
+    }
+
+    class WaitingStack extends Stack{
+        //被覆盖以通知那些被waitingPop所阻塞的线程
+        @Override
+        public synchronized void push(Object x) {
+            super.push(x);
+            notifyAll();
+        }
+        public synchronized Object waitingPop() throws InterruptedException{
+            while (isEmpty())wait();
+            try {
+                return super.pop();
+            }catch (StackEmptyException ie){
+                throw new Error("Internal implementation error");
+            }
+        }
+    }
+
+    //避免某些些做常见的扩展性之一就是：把保障和通知都封装在可覆盖的方法中，然后按照如下的形式构建公共方法：
+    /**
+     * public synchronized void anAction(){
+     *     awaitForThisAction();
+     *     doAction();
+     *     NotifyOtherByThisAction();
+     * }
+     */
+
+    //限制及嵌套的监视器(潜在死锁问题)
+    class PartWithGuard{
+        protected boolean cond=false;
+        //这里可能造成死锁，一个线程进来之后释放了锁处于等待状态，但是外部的锁一直跟随这个等待的线程没有被释放，所以外部的锁一直无法被其他线程获取
+        synchronized void await()throws InterruptedException{
+            while (!cond)wait();
+            //any other code
+        }
+        synchronized void signal(boolean c){
+            cond=c;
+            notifyAll();
+        }
+    }
+
+    class Host{
+        protected final PartWithGuard part=new PartWithGuard();
+        synchronized void rely()throws InterruptedException{
+            part.await();
+        }
+        synchronized void set(boolean c){
+            part.signal(c);
+        }
+    }
+    //上述方法中会出现的问题是：假设一个线程T调用了host.rely方法导致了其在part中被阻塞，而当T被阻塞时
+    //他所拥有的host的锁还是保持的，因此没有其他的线程能够通过执行host.set来解除这个锁
+    //当普通的同步方法调用其他一样普通但是却使用了wait的同步方法时，这种嵌套问题就可能会导致意想不到的的死锁
+
+    //有两个方法可以避免嵌套的监视器所带来的死锁问题，第一：在那些调用part对象方法的的host对象方法中不使用同步，
+    //当host对象的调用是无状态的时候就可以使用这种方法
+        //2：如果part对象的方法必须访问被锁定的host对象的状态，则可以重新定义part类，让其使用一种扩展形式的分层的容器锁
+        //这种方法将把host对象作为监视器，如下
+    class OwnedPartWithGuard{
+        protected boolean cond=false;
+        final Object lock;
+        OwnedPartWithGuard(Object x){lock=x;}
+        void await() throws InterruptedException{
+            synchronized (lock){
+                while (!cond)lock.wait();
+                //...
+            }
+        }
+        void signal(boolean c){
+            synchronized (lock){
+                cond=c;
+                lock.notifyAll();
+            }
+        }
+
+    }
 
 }
---
+++ src/main/java/com/master/PART3/UseConcurrentTools.java
@@ -0,0 +1,10 @@
+package com.master.PART3;
+
+/**
+ * @author ColorXJH
+ * @version 1.0
+ * @description: 使用并发工具类
+ * @date 2024-05-17 11:20
+ */
+public class UseConcurrentTools {
+}
