a73576f2b842a99aed4518486cd4c0f77d3447b7
ColorXJH <1204332316@qq.com>
2024-05-29 17:21:35 +0800

    238

--- src/main/java/com/master/PART4/CreateThread.java
+++ src/main/java/com/master/PART4/CreateThread.java
@@ -1,12 +1,15 @@
 package com.master.PART4;
 
+import java.awt.*;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
+import java.util.List;
 import java.util.Queue;
+import java.util.*;
 
 /**
  * @author ColorXJH
@@ -192,12 +195,124 @@ public class CreateThread {
 
     }
 
+    //轮询和事件驱动的io
+        //事件驱动的任务
+    class SessionTask implements Runnable{
+        private static final int BUFFSIZE=1024;
+        protected final Socket socket;
+        protected final InputStream input;
+        SessionTask(Socket s)throws IOException{
+            socket=s;
+            input=socket.getInputStream();
+        }
+        void processCommand(byte[] buffer,int bytes){}
+        void cleanup(){}
 
+        @Override
+        public void run() {
+            byte[] commandBuffer=new byte[BUFFSIZE];
+            try{
+              for(;;){
+                  int bytes=input.read(commandBuffer,0,BUFFSIZE);
+                  if(bytes!=BUFFSIZE){
+                      break;
+                  }
+                  processCommand(commandBuffer,bytes);
+              }
+            }
+            catch (IOException ie){
+                cleanup();
+            }
+            finally {
+                try {
+                    input.close();socket.close();
+                }catch (IOException ignore){
 
+                }
+            }
+        }
+    }
 
+    //一个基于会话的设计可以通过以下方式转换为一个事件驱动的设计
+        //1:隔离基本命令的功能，将其放在重新设计的任务的run方法中，每次先读取命令，然后再执行相关操作
+        //2：定义run方法，无论什么时候，当输入可读的时候，（或发生IO异常的情况下）它都可以被重复的触发
+        //3：手动维护完整的状态，从而在每个会话完成之后，因为输入已经被耗尽或连接已经被关闭，而实事件操作不再被触发
+    class IOEventTask implements Runnable{
+        private static final int BUFFSIZE=1024;
+        protected final Socket socket;
+        protected final InputStream inputStream;
+        protected volatile boolean done=false;
+        IOEventTask(Socket s)throws IOException{
+            socket=s;inputStream=socket.getInputStream();
+        }
+        void processCommand(byte[] buffer,int bytes){}
+        void cleanup(){}
+        boolean done(){return done;}
+        InputStream input(){return inputStream;}
+        @Override
+        public void run() {
+            if(done)return;
+            byte[]commandBuffer=new byte[BUFFSIZE];
+            try {
+                int bytes=inputStream.read(commandBuffer,0,BUFFSIZE);
+                if(bytes!=BUFFSIZE)done=true;
+                else processCommand(commandBuffer,bytes);
+            }catch (IOException ie){
+                cleanup();
+                done=true;
+            }
+            finally {
+                if(!done){return;}
+                try {
+                    inputStream.close();
+                    socket.close();
+                }catch (IOException IE){
 
+                }
+            }
+        }
+    }
 
+    //触发
+    class PollingWorker implements Runnable{
+        private List tasks=new ArrayList<>();
+        private long sleepTime;
+        void register(IOEventTask t){tasks.add(t);}
+        void deregister(IOEventTask t){tasks.remove(t);}
+        @Override
+        public void run() {
+            try{
+                for(;;){
+                    for(Iterator ir=tasks.iterator(); ir.hasNext();){
+                        IOEventTask t= (IOEventTask) ir.next();
+                        if(t.done)deregister(t);
+                        else{
+                            boolean  trigger;
+                            try{
+                                trigger=t.input().available()>0;
+                            }catch (IOException e){
+                                trigger=true;
+                            }
+                            if(trigger)t.run();
+                        }
+                    }
+                    Thread.sleep(sleepTime);
+                }
+            }catch (InterruptedException ie){
 
+            }
+        }
+    }
+
+    //消息表现
+    abstract class Box{
+        protected Color color=Color.white;
+        public synchronized Color getColor(){return color;}
+        public synchronized void setColor(Color c){color=c;}
+        public abstract Dimension size();
+        public abstract Box duplicate();
+        public abstract void show(Graphics g,Point origin);
+    }
 
 
 
